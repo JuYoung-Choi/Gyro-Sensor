e75e5ece9130324b8864c6c5a74f2b184a6377a1
DESKTOP-VHQ32AI\ELROIS <calewell@naver.com>
2017-09-07 10:50:09 +0900

    Gyro-Test
    Euler and Quaternion Test

---
+++ LookEuler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookEuler : MonoBehaviour {
+
+    float mouseSensitivity = 2.0f;
+    float rotLeftRight, rotTopBottom;
+
+    Vector3 m_PlayerRot;
+
+	RaycastHit temp;
+
+	void Start()
+	{
+		Input.gyro.enabled = true;
+	}
+
+	private void FixedUpdate()
+	{
+        #if UNITY_EDITOR //유니티 에디터에서는 마우스로 카메라 회전
+            FPRotate();
+        #elif UNITY_ANDROID //폰에서는 자이로센서로 카메라 회전
+            gyroupdate();   
+        #endif
+    }
+
+    //마우스 사용해서 카메라 회전
+    void FPRotate()
+    {
+        rotLeftRight = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotTopBottom += Input.GetAxis("Mouse Y") * mouseSensitivity;
+        rotTopBottom = Mathf.Clamp(rotTopBottom, -20.0f, 60.0f);
+
+        transform.localEulerAngles = new Vector3(-rotTopBottom, rotLeftRight, 0);
+    }
+
+    //오일러와 자이로 센서
+    void gyroupdate()
+	{
+		m_PlayerRot.x -= Input.gyro.rotationRate.x;
+		m_PlayerRot.y += Input.gyro.rotationRate.y * -1f;
+
+		if (m_PlayerRot.x >= 360f)
+			m_PlayerRot.x = 0f;
+
+		if (m_PlayerRot.y >= 360f)
+			m_PlayerRot.y = 0f;
+
+		transform.eulerAngles = m_PlayerRot;
+	}
+
+	//자이로 카메라 리셋
+	public void AngleReset()
+	{
+		m_PlayerRot = Vector3.zero;
+    }
+}
---
+++ LookQuat.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookQuat : MonoBehaviour {
+
+    float mouseSensitivity = 2.0f;
+    float rotLeftRight, rotTopBottom;
+
+    Quaternion Quat = Quaternion.identity;
+
+    void Start () {
+		Input.gyro.enabled = true;
+		Quat = Input.gyro.attitude;
+	}
+
+    private void FixedUpdate()
+	{
+        #if UNITY_EDITOR //유니티 에디터에서는 마우스로 카메라 회전
+            FPRotate();
+        #elif UNITY_ANDROID //폰에서는 자이로센서로 카메라 회전
+		    gyroupdate();
+        #endif
+    }
+
+    //마우스 사용해서 카메라 회전
+    void FPRotate()
+    {
+        rotLeftRight = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotTopBottom += Input.GetAxis("Mouse Y") * mouseSensitivity;
+        rotTopBottom = Mathf.Clamp(rotTopBottom, -20.0f, 60.0f);
+
+        transform.localEulerAngles = new Vector3(-rotTopBottom, rotLeftRight, 0);
+    }
+
+    //쿼터니온과 자이로센서
+    void gyroupdate()
+    {
+        transform.localRotation = Quaternion.Inverse(Quaternion.Inverse(Quat) * Input.gyro.attitude);
+    }
+
+    //자이로 카메라 리셋
+    public void ResetBtn(){
+		Quat = Input.gyro.attitude;	
+	}
+}
